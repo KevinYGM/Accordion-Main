4308e315043bc1d093ef8b16aae1eeca087987c8
KevinYGM <kvngonzalez35@gmail.com>
2024-09-22 18:17:23 +0200

    creation of array of questions and preview of the deployment of the array in the app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import star from './assets/icon-star.svg';
 import buttonPlus from './assets/icon-plus.svg';
 import buttonMinus from './assets/icon-minus.svg';
 import { useState } from 'react';
+import faqData from './faqData';
 
 function App() {
 
@@ -27,61 +28,23 @@ function App() {
         </div>
 
 
-        {/* First Question */}
-        <div className="question">
+        {/* Question */}
+
+        {faqData.map(data => (
+          <div key={data.id} className="question">
           <h2>
-            What is Frontend Mentor, and how will it help me?
+            {data.question}
           </h2>
 
           <img  src={stateButton ? buttonMinus : buttonPlus} 
                 alt="Button for open or Closed" 
                 onClick={ openClosed }/>
           
-          {openInfo && (
-              <p>
-                Frontend Mentor offers realistic coding challenges to help developers improve their 
-                frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for 
-                all levels and ideal for portfolio building.
-              </p>
-            )
-          }
+          <div className={`accordion ${openInfo ? 'open' : ''}`}>
+            <p>{data.answer}</p>
+          </div>
         </div>
-        
-
-        
-
-
-        {/* Second Question */}
-        <h2>
-          Is Frontend Mentor free?
-        </h2>
-
-        <p> Yes, Frontend Mentor offers both free and premium coding challenges, with the free 
-        option providing access to a range of projects suitable for all skill levels.</p>
-
-        
-
-        {/* Third Question */}
-        <h2>
-          Can I use Frontend Mentor projects in my portfolio?
-        </h2>
-
-        <p>
-          Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
-          way to showcase your skills to potential employers!
-        </p>
-
-
-
-        {/* Fourth Question */}
-        <h2>
-          How can I get help if I'm stuck on a Frontend Mentor challenge?
-        </h2>
-
-        <p>
-          The best place to get help is inside Frontend Mentor's Discord community. There's a help 
-          channel where you can ask questions and seek support from other community members.
-        </p>
+       ))}
       </div>
     </>
   );
