d7fbb6d3450238a083c3aea58dbc549d9e2df272
KevinYGM <kvngonzalez35@gmail.com>
2024-09-22 18:34:57 +0200

    Culmination of dynamic programming of the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import './App.css';
+import { useState } from 'react';
 import star from './assets/icon-star.svg';
 import buttonPlus from './assets/icon-plus.svg';
 import buttonMinus from './assets/icon-minus.svg';
-import { useState } from 'react';
 import faqData from './faqData';
 
 function App() {
+  const [openInfo, setOpenInfo] = useState(Array(faqData.length).fill(false));
 
-  const [stateButton, setStateButton] = useState(false);
-  const [openInfo, setOpenInfo] = useState(false);
-
-  const openClosed = () => {
-    setStateButton(prevState => !prevState);
-    setOpenInfo(prevState => !prevState);
-  }
+  const toggleInfo = (index) => {
+    setOpenInfo(prevState => {
+      const newState = [...prevState];
+      newState[index] = !newState[index];
+      return newState;
+    });
+  };
 
   return (
     <>
@@ -23,28 +24,26 @@ function App() {
 
         {/* Title */}
         <div className='title'>
-          <img src={star} alt='Star'/>
+          <img src={star} alt='Star' />
           <h1>FAQs</h1>
         </div>
 
+        {/* Questions */}
+        {faqData.map((data, index) => (
+          <div key={index} className="question">
+            <h2>{data.question}</h2>
 
-        {/* Question */}
-
-        {faqData.map(data => (
-          <div key={data.id} className="question">
-          <h2>
-            {data.question}
-          </h2>
-
-          <img  src={stateButton ? buttonMinus : buttonPlus} 
-                alt="Button for open or Closed" 
-                onClick={ openClosed }/>
-          
-          <div className={`accordion ${openInfo ? 'open' : ''}`}>
-            <p>{data.answer}</p>
+            <img 
+              src={openInfo[index] ? buttonMinus : buttonPlus} 
+              alt="Button for open or closed" 
+              onClick={() => toggleInfo(index)} 
+            />
+            
+            <div className={`accordion ${openInfo[index] ? 'open' : ''}`}>
+              <p>{data.answer}</p>
+            </div>
           </div>
-        </div>
-       ))}
+        ))}
       </div>
     </>
   );
